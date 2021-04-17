0b4aa825a7d3ab0bc11ced6148e899ca026d3119
pilpod <giacomo.corso@gmail.com>
2021-04-17 09:58:24 +0200

    function ones changed with loop

--- src/Yatzy.php
+++ src/Yatzy.php
@@ -39,16 +39,12 @@ class Yatzy
     public static function ones($d1, $d2, $d3, $d4, $d5)
     {
         $sum = 0;
-        if ($d1 == 1)
-            $sum += 1;
-        if ($d2 == 1)
-            $sum += 1;
-        if ($d3 == 1)
-            $sum += 1;
-        if ($d4 == 1)
-            $sum += 1;
-        if ($d5 == 1)
-            $sum += 1;
+        $dados = [$d1, $d2, $d3, $d4, $d5];
+
+        foreach ($dados as $dado) {
+            if ($dado == 1)
+                $sum += 1;
+        }
 
         return $sum;
     }
